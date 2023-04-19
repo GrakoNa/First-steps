5ee496265d8e5629160497c903de7d484fee035d
GrakoNa <grankinaalex@gmaail.com>
2023-04-19 12:04:24 +0300

    hi

--- Sol3/scripts/deploy.js
+++ Sol3/scripts/deploy.js
@@ -1,9 +1,14 @@
 // imports
 
+const {
+  TASK_FLATTEN_GET_FLATTENED_SOURCE,
+} = require("hardhat/builtin-tasks/task-names");
+
 // async main
 async function main() {}
 
 // main
+TASK_FLATTEN_GET_FLATTENED_SOURCE;
 main()
   .then(() => process.exit(0))
   .catch((error) => {
